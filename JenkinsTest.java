9184f588a8729f48c35eee734cdb14707169e858
AlmondKing <39296398+AlmondKing@users.noreply.github.com>
2023-05-11 19:31:42 +0530

    Update JenkinsTest.java

--- JenkinsTest.java
+++ JenkinsTest.java
@@ -4,7 +4,7 @@ public class JenkinsTest {
 	
 	public static void main(String Args[])
 	{
-		System.out.println("**** AGAIN MADE SOME CHANGESSSS!!");
+		System.out.println("***Webhook Jenkins Test**");
 	}
 
 }
